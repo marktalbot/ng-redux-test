b270ebd17d7040db71ec503a598502cdbb6f23dd
Mark Talbot <mark.talbot@experiencepoint.com>
2016-11-11 10:34:45 -0500

    Updating to Angular 2.1.0 and newest version of ng cli

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,9 +1,11 @@
-export class ProjectPage {
+import { browser, element, by } from 'protractor';
+
+export class NgReduxPage {
   navigateTo() {
     return browser.get('/');
   }
 
   getParagraphText() {
-    return element(by.css('project-app h1')).getText();
+    return element(by.css('app-root h1')).getText();
   }
 }
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,2 @@
-export {environment} from './environment';
-export {ProjectAppComponent} from './project.component';
+export * from './app.component';
+export * from './app.module';
